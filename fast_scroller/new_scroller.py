fb9584d3acf59f66bdfd9a9ba7e641c9e07e5825
miketrumpis <mtrumpis@gmail.com>
2017-05-01 17:16:33 -0400

    updates

--- fast_scroller/new_scroller.py
+++ fast_scroller/new_scroller.py
@@ -19,15 +19,16 @@ import h5py
 import PySide
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph_extensions as pgx
 
 from traits.api import Range, Instance, Button, HasTraits, File, Float, Str
 from traitsui.api import View, VGroup, HGroup, Item, UItem, CustomEditor, \
      Label
 
 from ecogana.devices.electrode_pinouts import get_electrode_map
+from ecoglib.util import ChannelMap
 
-
-def h5mean(array, axis, block_size=20000):
+def h5mean(array, axis, block_size=20000, start=0, stop=-1):
     """Compute mean of a 2D HDF5 array in blocks"""
 
     shape = array.shape
@@ -41,7 +42,10 @@ def h5mean(array, axis, block_size=20000):
         # to compute block x, either slice like:
         # array[:, x].mean(0)
         # array[x, :].mean(1)
-        sl = ( range, slice(None) ) if axis else ( slice(None), range )
+        if axis == 1:
+            sl = ( range, slice(start, stop) )
+        else:
+            sl = ( slice(start, stop), range )
         mn[range] = array[sl].mean(axis)
         b += block_size
         if b >= mn_size:
@@ -50,7 +54,14 @@ def h5mean(array, axis, block_size=20000):
 
             
 class ReadCache(object):
+    """
+    Buffers row indexes from memmap or hdf5 file.
 
+    For cases where array[0, n:m], array[1, n:m], array[2, n:m] are
+    accessed sequentially, this object buffers the C x (m-n)
+    submatrix before yielding individual rows.
+    """
+    
     def __init__(self, array):
         self._array = array
         self._current_slice = None
@@ -70,6 +81,57 @@ class ReadCache(object):
         new_sl = (indx, new_range)
         return self._current_seg[new_sl].copy()
 
+class FilteredReadCache(ReadCache):
+    """
+    Apply row-by-row filters to a ReadCache
+    """
+
+    def __init__(self, array, filters):
+        if not isinstance(filters, (tuple, list)):
+            f = filters
+            filters = [ f ] * len(array)
+        self.filters = filters
+        super(FilteredReadCache, self).__init__(array)
+
+    def __getitem__(self, sl):
+        idx = sl[0]
+        x = super(FilteredReadCache, self).__getitem__(sl)
+        return self.filters[idx]( x )
+
+def make_filt(z):
+    def _f(x):
+        return x - z
+    return _f
+    
+class DCOffsetReadCache(FilteredReadCache):
+    """
+    A filtered read cache with a simple offset subtraction.
+    """
+
+    def __init__(self, array, offsets):
+        #filters = [lambda x: x - off for off in offsets]
+        filters = [make_filt(off) for off in offsets]
+        super(DCOffsetReadCache, self).__init__(array, filters)
+        self.offsets = offsets
+
+class HMSAxis(pg.AxisItem):
+
+    def _to_hms(self, x):
+        h = x // 3600
+        m = (x - h*3600) // 60
+        s = x - h * 3600 - m * 60
+        return map(int, (h, m, s))
+    
+    def tickStrings(self, values, scale, spacing):
+        strns = []
+        if len(values)==0:
+            return super(HMSAxis, self).tickStrings(values,scale,spacing)
+
+        for x in values:
+            strns.append('{0:02d}:{1:02d}:{2:02d}'.format(*self._to_hms(x)))
+        return strns
+
+        
 class HDF5Plot(pg.PlotCurveItem):
     def __init__(self, *args, **kwds):
         self.hdf5 = None
@@ -86,80 +148,65 @@ class HDF5Plot(pg.PlotCurveItem):
         
     def viewRangeChanged(self):
         self.updateHDF5Plot()
+
+    def _view_range(self):
+        vb = self.parentWidget()
+        if vb is None:
+            return  # no ViewBox yet
+        # Determine what data range must be read from HDF5
+        xrange = self.parentWidget().viewRange()[0]
+        srange = map(lambda x: int(x / self._xscale), xrange)
+        start = max(0,int(srange[0])-1)
+        stop = min(self.hdf5.shape[-1], int(srange[1]+2))
+        return start, stop
+        
+    def _set_data(self):
+        r = self._view_range()
+        if not r:
+            return
+        start, stop = r
+        self.x_visible = np.arange(start, stop) * self._xscale
+        self.y_visible = self.hdf5[self.index, start:stop]
+        self.y_visible *= self._yscale
         
     def updateHDF5Plot(self):
         if self.hdf5 is None:
             self.setData([])
             return
         
-        #vb = self.getViewBox()
-        vb = self.parentWidget()
-        if vb is None:
-            return  # no ViewBox yet
-        
-        # Determine what data range must be read from HDF5
-        #xrange = vb.viewRange()[0]
-        xrange = self.parentWidget().viewRange()[0]
-        srange = map(lambda x: int(x / self._xscale), xrange)
-        start = max(0,int(srange[0])-1)
-        stop = min(self.hdf5.shape[-1], int(srange[1]+2))
-        x_visible = np.arange(start, stop) * self._xscale
-        
+        self._set_data()
+        r = self._view_range()
+        if not r:
+            return
+        start, stop = r
         # Decide by how much we should downsample 
         ds = int((stop-start) / self.limit) + 1
 
         if ds == 1:
             # Small enough to display with no intervention.
-            visible = self.hdf5[self.index, start:stop]
-            visible *= self._yscale
-            visible += self.offset
-            scale = 1
+            visible = self.y_visible
+            x_visible = self.x_visible
+            ## visible += self.offset
         else:
             # Here convert data into a down-sampled array suitable for visualizing.
-            # Must do this piecewise to limit memory usage.        
-            samples = 1 + ((stop-start) // ds)
-            visible = np.zeros(samples*2, dtype=self.hdf5.dtype)
-            x_samp = np.linspace(ds//2, len(x_visible)-ds//2, samples*2).astype('i')
-            while x_samp[-1] >= len(x_visible):
-                x_samp = x_samp[:-1]
-            x_visible = np.take(x_visible, x_samp)
-            sourcePtr = start
-            targetPtr = 0
-            
-            # read data in chunks of ~1M samples
-            chunkSize = (1000000//ds) * ds
-            while sourcePtr < stop-1:
-                sl = ( self.index,
-                       slice(sourcePtr, min(stop,sourcePtr+chunkSize)) )
-                chunk = self.hdf5[sl]
-                sourcePtr += len(chunk)
-                
-                # reshape chunk to be integral multiple of ds
-                chunk = chunk[:(len(chunk)//ds) * ds].reshape(len(chunk)//ds, ds)
-                
-                # compute max and min
-                chunkMax = chunk.max(axis=1)
-                chunkMin = chunk.min(axis=1)
-                
-                # interleave min and max into plot data to preserve envelope shape
-                visible[targetPtr:targetPtr+chunk.shape[0]*2:2] = chunkMin
-                visible[1+targetPtr:1+targetPtr+chunk.shape[0]*2:2] = chunkMax
-                targetPtr += chunk.shape[0]*2
-            
-            visible = visible[:targetPtr]
-            x_visible = x_visible[:targetPtr]
-            visible *= self._yscale
-            #visible += self.offset
-            scale = ds * 0.5
 
-        visible = detrend(visible, type='constant') + self.offset
+            N = len(self.y_visible)
+            Nsub = N // ds
+            y = self.y_visible[:Nsub * ds].reshape(Nsub, ds)
+            visible = np.empty(Nsub*2, dtype=self.hdf5.dtype)
+            visible[0::2] = y.min(axis=1)
+            visible[1::2] = y.max(axis=1)
+
+            x_samp = np.linspace(ds//2, len(self.x_visible)-ds//2, Nsub*2).astype('i')
+            if x_samp[-1] >= len(self.x_visible):
+                x_samp[-1] = N-1
+            x_visible = np.take(self.x_visible, x_samp)
+             
+        #visible = detrend(visible, type='constant') + self.offset
+        visible = visible + self.offset
         self.setData(x=x_visible, y=visible) # update the plot
-        #self.setData(visible + self.index * offset)
         #self.setPos(start*self._xscale, 0) # shift to match starting index
         self.resetTransform()
-        #self.scale(scale, 1)  # scale to match downsampling
-
-#pg.mkQApp()
 
 class FastScroller(object):
 
@@ -167,15 +214,37 @@ class FastScroller(object):
                  load_channels='all', max_zoom=120.0, units='mV'):
         
 
+        # Set up data access
+        f = h5py.File(file_name, 'r')
+        nchan = f['data'].shape[0]
+        del_t = f['Fs'].value ** -1.0
+        # array = ReadCache(f['data'][:, :100000])
+        # scale = 1e3 / 20 # uV?
+        mn_trace = h5mean(f['data'], 0) * scale
+        mn_level = h5mean(f['data'], 1, start=int(3e4))
+        if isinstance(load_channels, str) and load_channels.lower() == 'all':
+            load_channels = xrange(nchan)
+            self.chan_map = chan_map
+        elif len(load_channels) <= len(chan_map):
+            new_cm = ChannelMap( [chan_map[i] for i in load_channels],
+                                  chan_map.geometry,
+                                  col_major=chan_map.col_major )
+            self.chan_map = new_cm
+        else:
+            raise ValueError('cannot map the listed channels')
+
+        array = DCOffsetReadCache(f['data'], mn_level)
+        self.array = array
+
         #self.win = pg.GraphicsWindow()
         self.win = pg.GraphicsView()
         layout = pg.GraphicsLayout(border=(10,10,10))
         #sub_layout = layout.addLayout(row=0, col=0, rowspan=2, colspan=3)
         self.win.setCentralItem(layout)
-
-        
-        self.chan_map = chan_map
-
+        layout.addLabel('Array heatmap')
+        layout.addLabel('Zoomed plot')
+        layout.addLabel('|')
+        layout.nextRow()
         # win.setWindowTitle('pyqtgraph example: HDF5 big data')
         # set up a 3x3 grid:
         # __.__.__
@@ -188,24 +257,35 @@ class FastScroller(object):
         ## sub_layout.nextCol()
         ## sub_layout.nextCol()
         #layout.nextRow()
-        self.p1 = layout.addPlot(colspan=2, row=0, col=0, rowspan=2)
+        sub_layout = layout.addLayout(colspan=1)
+        self.p_img = sub_layout.addViewBox(lockAspect=True)
+
+        ## self.img = pg.ImageItem(np.random.randn(*self.chan_map.geometry))
+        self.img = pgx.ImageItem(np.random.randn(*self.chan_map.geometry).T)
+        self.img.setLookupTable(pgx.get_colormap_lut(name='bipolar'))
+        self.p_img.addItem(self.img)
+        self.p_img.autoRange()
+        self.img.setLevels( (-3, 3) )
+        self.cb = pgx.ColorBarItem(
+            image=self.img, label='Voltage ({0})'.format(units)
+            )
+        self.img.setLevels( (-10, 10) )
+        sub_layout.addItem(self.cb)
+
+        self.p1 = layout.addPlot(colspan=2, row=1, col=1)
         self.p1.enableAutoRange(False, False)
         self.p1.setLabel('left', 'Amplitude', units=units)
         self.p1.setLabel('bottom', 'Time', units='s')
 
         # The image panel
-        self.p_img = layout.addViewBox(lockAspect=True, row=0, col=2)
-        #self.p_img = layout.addPlot(col=4, rowspan=2, rowspan=2)
-        self.img = pg.ImageItem(np.random.randn(*self.chan_map.geometry))
-        self.p_img.addItem(self.img)
-        self.p_img.autoRange()
-
         layout.nextRow()
 
         # The navigator plot
-        self.p2 = layout.addPlot(row=2, col=0, colspan=3)
+        axis = HMSAxis(orientation='bottom')
+        self.p2 = layout.addPlot(row=2, col=0, colspan=3,
+                                 axisItems={'bottom':axis})
         self.p2.setLabel('left', 'Amplitude', units=units)
-        self.p2.setLabel('bottom', 'Time', units='s')
+        self.p2.setLabel('bottom', 'Time')#, units='s')
         self.region = pg.LinearRegionItem() 
         self.region.setZValue(10)
 
@@ -217,20 +297,11 @@ class FastScroller(object):
         self.p1.setAutoVisible(y=True)
 
 
-        # Add traces to top plot
-        f = h5py.File(file_name, 'r')
-        nchan = f['data'].shape[0]
-        del_t = f['Fs'].value ** -1.0
-        array = ReadCache(f['data'])
-        # array = ReadCache(f['data'][:, :100000])
-        # scale = 1e3 / 20 # uV?
 
         self.p1.setXRange(0, 500*del_t)
         self.p1.vb.setLimits(maxXRange=max_zoom)
 
-        if isinstance(load_channels, str) and load_channels.lower() == 'all':
-            load_channels = xrange(nchan)
-
+        # curves will be itemized in order with the channel map
         self._curves = []
         for i in load_channels:
             curve = HDF5Plot()
@@ -239,10 +310,8 @@ class FastScroller(object):
             self._curves.append( curve )
 
         # Add mean trace to bottom plot
-        mn = h5mean(f['data'], 0) * scale
-        self.p2.plot(x=np.arange(len(mn))*del_t, y=mn)
-        self.p2.setXRange(0, min(5e4, len(mn))*del_t)
-
+        self.p2.plot(x=np.arange(len(mn_trace))*del_t, y=mn_trace)
+        self.p2.setXRange(0, min(5e4, len(mn_trace))*del_t)
 
         # Set bidirectional plot interaction
         self.region.sigRegionChanged.connect(self.update)
@@ -263,12 +332,24 @@ class FastScroller(object):
         minX, maxX = self.region.getRegion()
         rng = 0.5 * (maxX - minX)
         self.region.setRegion( [ newX - rng, newX + rng ] )
+
+    def set_mean_image(self):
+        if not hasattr(self._curves[0], 'y_visible'):
+            return
+        image = np.empty( len(self.chan_map), 'd' )
+        for i in xrange(len(self.chan_map)):
+            image[i] = self._curves[i].y_visible.mean()
+        self.img.setImage( self.chan_map.embed(image, fill=0).T,
+                           autoLevels=False )
         
     def update(self):
         self.region.setZValue(10)
         minX, maxX = self.region.getRegion()
-        self.p1.setXRange(minX, maxX, padding=0)    
-
+        # update zoom plot
+        self.p1.setXRange(minX, maxX, padding=0)
+        # update image with mean of the current view interval
+        self.set_mean_image()
+        
 
     def updateRegion(self, window, viewRange):
         rgn = viewRange[0]
