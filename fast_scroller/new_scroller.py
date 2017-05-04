c100456315cd51d928adae7de3b918c3c3d98cc4
miketrumpis <mtrumpis@gmail.com>
2017-05-04 15:15:04 -0400

    module-ify graph manipulations

--- fast_scroller/new_scroller.py
+++ fast_scroller/new_scroller.py
@@ -1,13 +1,9 @@
 from __future__ import division
 
-import matplotlib
-matplotlib.use('Qt4Agg')
-import matplotlib.pyplot as pp
-if __name__=='__main__':
-    from matplotlib.figure import Figure
-    pyplot_figure = pp.figure
-    pp.figure = Figure
-
+import matplotlib as mpl
+mpl.rcParams['interactive'] = True
+mpl.use('Qt4Agg')
+from matplotlib.figure import Figure
 import sys
 import numpy as np
 import PySide
@@ -27,11 +23,35 @@ from ecoglib.estimation.jackknife import Jackknife
 from ecoglib.numutil import nextpow2
 from sandbox.split_methods import multi_taper_psd
 
-import seaborn as sns
+import ecogana.anacode.seaborn_lite as sns
 
-class PlotsInterval(HasTraits):
+
+## from matplotlib.backends.backend_qt4agg import \
+##      FigureCanvasQTAgg as FigureCanvas
+## from matplotlib.backends.backend_qt4agg import \
+##      NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt4agg import new_figure_manager_given_figure
+def simplefigure(fig):
+    from pyqtgraph.Qt import QtCore
+    canvas = FigureCanvas(fig)
+    main_frame = QtGui.QWidget()
+    canvas.setParent(main_frame)
+    mpl_toolbar = NavigationToolbar(canvas, main_frame)        
+    vbox = QtGui.QVBoxLayout()
+    vbox.addWidget(canvas)
+    vbox.addWidget(mpl_toolbar)
+    main_frame.setLayout(vbox)
+    #main_frame.setGeometry(QtCore.QRect(100, 100, 400, 200))
+    return main_frame
+
+
+
+class VisModule(HasTraits):
     name = Str('__dummy___')
     parent = Instance('VisWrapper')
+
+class PlotsInterval(VisModule):
+    pass
 
 class IntervalSpectrum(PlotsInterval):
     name = 'Power Spectrum'
@@ -42,6 +62,7 @@ class IntervalSpectrum(PlotsInterval):
     adaptive = Bool(True)
     new_figure = Bool(False)
     plot = Button('Plot')
+    _figures = List([])
 
     def __default_mtm_kwargs(self, n):
         kw = dict()
@@ -61,11 +82,13 @@ class IntervalSpectrum(PlotsInterval):
     def _get_fig(self):
         fig = getattr(self, 'fig', None)
         if fig and not self.new_figure:
-            return fig, fig.axes[0], pp.draw
-        fig, ax = pp.subplots()
+            print 'returning existing figure'
+            return fig, fig.axes[0], fig.canvas.draw_idle
+        self.fig = Figure()
+        ax = self.fig.add_subplot(111)
         self._plot_count = 0
-        self._colors = sns.color_palette()
-        return fig, ax, pp.show
+        self._colors = mpl.rcParams["axes.prop_cycle"].by_key()["color"]
+        return self.fig, ax, None
          
     def _plot_fired(self):
         x, y = self.parent._qtwindow.current_data()
@@ -81,12 +104,14 @@ class IntervalSpectrum(PlotsInterval):
             if not self.sem:
                 # get jackknife stdev
                 se = jn.estimate(np.std)
-            print mn.shape, se.shape
+            ## l_pxx = np.log(pxx)
+            ## mn = l_pxx.mean(0)
+            ## se = l_pxx.std(0) / np.sqrt(len(l_pxx))
+            se = np.exp( mn - se ), np.exp( mn + se )
             filled_interval(
-                ax.semilogy, fx, np.exp(mn), np.exp(se),
+                ax.semilogy, fx, np.exp(mn), se,
                 color=self._colors[self._plot_count], label=label, ax=ax
                 )
-            self._plot_count += 1
             ax.legend()
         else:
             lns = ax.semilogy(
@@ -94,7 +119,15 @@ class IntervalSpectrum(PlotsInterval):
                 )
             self._plot_count += 1
             ax.legend(lns[:1], (label,))
-        update()
+        sns.despine(ax=ax)
+        if self._plot_count == 0:
+            splot = SavesFigure.live_fig(fig, has_graphs=True)
+            #splot = simplefigure(fig)
+            #splot.show()
+            self._figures.append(splot)
+        self._plot_count += 1
+        if update is not None:
+            update()
 
     def default_traits_view(self):
         v = View(
@@ -115,18 +148,89 @@ class IntervalSpectrum(PlotsInterval):
             )
         return v
 
+class YRange(VisModule):
+    name = 'Trace Offsets'
+    y_spacing = Enum(0.5, [0.1, 0.2, 0.5, 1, 2, 5])
+
+    @on_trait_change('y_spacing')
+    def _change_spacing(self):
+        self.parent._qtwindow.update_y_spacing(self.y_spacing)
+
+    def default_traits_view(self):
+        v = View(
+            Group(
+                UItem('y_spacing'),
+                label='Trace spacing (mv)'
+                )
+            )
+        return v
+
+class AnimateInterval(VisModule):
+    name = 'Animate window'
+    anim_frame = Button('Animate')
+    anim_time_scale = Enum(50, [0.5, 1, 5, 10, 20, 50, 100])
+
+    def __step_frame(self):
+        n = self.__n
+        x = self.__x
+        y = self.__y
+        if n >= self.__n_frames:
+            self._atimer.Stop()
+            return
+        t0 = time.time()
+        scaled_dt = self.anim_time_scale * (x[1] - x[0])
+        try:
+            self.parent._qtwindow.set_image_frame(x=x[n], frame_vec=y[:,n])
+        except IndexError:
+            self._atimer.Stop()
+        PySide.QtCore.QCoreApplication.instance().processEvents()
+        # calculate the difference between the desired interval
+        # and the time it just took to draw (per frame)
+        t_pause = scaled_dt - (time.time() - t0) / self.__f_skip
+        if t_pause < 0:
+            self.__f_skip += 1
+            print 'took', -t_pause, 'sec too long, increasing skip rate', self.__f_skip
+            sys.stdout.flush()
+        else:
+            print t_pause
+            self._atimer.setInterval(t_pause * 1000.0)
+        self.__n += self.__f_skip
+
+    def _anim_frame_fired(self):
+        if hasattr(self, '_atimer') and self._atimer.IsRunning():
+            self._atimer.Stop()
+            return
+        
+        x, self.__y = self.parent._qtwindow.current_data()
+        self.__f_skip = 1
+        self.__x = x[0]
+        dt = self.__x[1] - self.__x[0]
+        self.__n_frames = self.__y.shape[1]
+        self.__n = 0
+        print self.anim_time_scale * dt * 1000
+        print self.__n_frames
+        self._atimer = Timer( self.anim_time_scale * dt * 1000,
+                              self.__step_frame )
+
+    def default_traits_view(self):
+        v = View(
+            VGroup(
+                Item('anim_time_scale', label='Divide real time'),
+                Item('anim_frame'),
+                label='Animate Frames'
+                )
+            )
+        return v
+    
 def setup_qwidget_control(parent, editor, qwidget):
     return qwidget
 
 class VisWrapper(HasTraits):
 
     graph = Instance(QtGui.QWidget)
-    anim_frame = Button('Animate')
-    anim_time_scale = Enum(50, [0.5, 1, 5, 10, 50, 100])
-    y_spacing = Enum(0.5, [0.1, 0.2, 0.5, 1, 2, 5])
     x_scale = Float
 
-    modules = List( [IntervalSpectrum] )
+    modules = List( [YRange, AnimateInterval, IntervalSpectrum] )
 
     def __init__(self, qtwindow, **traits):
         self._qtwindow = qtwindow
@@ -139,75 +243,17 @@ class VisWrapper(HasTraits):
         for m in mods:
             self.modules.append( m(parent=self) )
 
-    def __step_frame(self):
-        n = self.__n
-        x = self.__x
-        y = self.__y
-        if n >= self.__n_frames:
-            self._atimer.Stop()
-            return
-        t0 = time.time()
-        scaled_dt = self.anim_time_scale * (x[1] - x[0])
-        try:
-            self._qtwindow.set_image_frame(x=x[n], frame_vec=y[:,n])
-        except IndexError:
-            self._atimer.Stop()
-        PySide.QtCore.QCoreApplication.instance().processEvents()
-        t_pause = scaled_dt - (time.time() - t0)
-        if t_pause < 0:
-            self.__f_skip += 1
-            sys.stdout.flush()
-        else:
-            self._atimer.setInterval(t_pause * 1000.0)
-        self.__n += self.__f_skip
-
-    def _anim_frame_fired(self):
-        if hasattr(self, '_atimer') and self._atimer.IsRunning():
-            self._atimer.Stop()
-            return
-        
-        x, self.__y = self._qtwindow.current_data()
-        self.__f_skip = 1
-        self.__x = x[0]
-        dt = self.__x[1] - self.__x[0]
-        self.__n_frames = self.__y.shape[1]
-        self.__n = 0
-        print self.anim_time_scale * dt * 1000
-        print self.__n_frames
-        self._atimer = Timer( self.anim_time_scale * dt * 1000,
-                              self.__step_frame )
-
-    @on_trait_change('y_spacing')
-    def _change_spacing(self):
-        self._qtwindow.update_y_spacing(self.y_spacing)
-
     def default_traits_view(self):
         v = View(
             VGroup(
                 UItem('graph',
                       editor=CustomEditor(setup_qwidget_control,
                                           self._qtwindow.win)),
-                HGroup(
-                    VGroup(
-                        Item('anim_time_scale', label='Divide real time'),
-                        Item('anim_frame'),
-                        label='Animate Frames'
-                        ),
-                    VGroup(
-                        UItem('y_spacing'),
-                        label='Trace offset (mV)'
-                        ),
-                    Group(
-                        UItem('modules', style='custom',
-                             editor=ListEditor(
-                             use_notebook=True, deletable=False, 
-                             dock_style='tab', page_name='.name',
-                             ), height=-200
-                        ),
-                        show_border=True,
-                        label='Modules'
-                        )
-                    ),
+                UItem('modules', style='custom',
+                      editor=ListEditor(use_notebook=True, deletable=False, 
+                                        dock_style='tab', page_name='.name'),
+                                        
+                      height=-150),
                 ),
             width=1200,
             height=500,
