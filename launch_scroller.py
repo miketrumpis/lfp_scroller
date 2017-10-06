84fb6119f90bb04c1de2a0ef164902cc5e17bf76
miketrumpis <mtrumpis@gmail.com>
2017-10-06 11:58:18 -0400

    force use of qt4agg

--- launch_scroller.py
+++ launch_scroller.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import matplotlib
+matplotlib.use('Qt4Agg')
 from fast_scroller.loading import VisLauncher
 
 if __name__ == '__main__':
-    v = VisLauncher(headstage='mux7', chan_map='ratv4_mux6')
-    v.configure_traits()  
+    v = VisLauncher(headstage='mux7', chan_map='ratv4_mux6_15row')
+    v.configure_traits()
