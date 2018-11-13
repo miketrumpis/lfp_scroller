8315345591073cf276bd7ed299a6adbceb305d78
miketrumpis <mtrumpis@gmail.com>
2018-11-13 13:32:43 -0800

    proper setup syntax

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='lfp_scroller',
     version='0.1',
-    packages='fast_scroller',
-    scripts='launch_scroller.py'
+    packages=['fast_scroller'],
+    scripts=['launch_scroller.py']
     )
