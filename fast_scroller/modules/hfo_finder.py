0c979e194516d4257420b4bc429b0b04fd3c71d0
miketrumpis <mtrumpis@gmail.com>
2021-04-28 17:57:40 -0700

    start an HFO plot with scalogram

--- fast_scroller/modules/hfo_finder.py
+++ fast_scroller/modules/hfo_finder.py
@@ -1,14 +1,100 @@
 from collections import OrderedDict
 import numpy as np
 import pandas as pd
-from traits.api import Int, Float, Array, Range, Button, Bool, Str, File, on_trait_change
+from scaleogram import CWT, cws
+from matplotlib.gridspec import GridSpec
+from matplotlib.ticker import NullFormatter, FuncFormatter
+from scipy.signal import detrend
+from traits.api import Int, Float, Array, Range, Button, Bool, Str, File, observe, Instance, Tuple
 from traitsui.api import View, HGroup, VGroup, UItem, Label, TabularEditor
 from traitsui.tabular_adapter import TabularAdapter
 
-from .base import VisModule
+from ecogdata.filt.time import slepian_projection
+from ecogdata.devices.units import nice_unit_text
+from ecoglib.vis import plotters
+
+from .base import PlotsInterval, GridSpecStack
 from ..curve_collections import PlotCurveCollection, SelectedFollowerCollection
 
 __all__ = ['HFOLocator']
+
+
+@FuncFormatter
+def _sec_to_msec(sec, pos):
+    return '{:d}'.format(int(sec * 1000))
+
+
+def hfo_plot(lfp: np.ndarray, full_window: tuple, hfo_window: tuple, gridspec: GridSpec,
+             hfo_band: tuple=(80, 500),
+             normalize: bool=True):
+    # times are given and displayed in milliseconds
+    start, end = [t / 1000.0 for t in full_window]
+    hfo_start, hfo_end = [t / 1000.0 for t in hfo_window]
+    win_size = len(lfp)
+    dt = (end - start) / win_size
+    lfp_flat = detrend(lfp, type='linear')
+    hfo_time = np.arange(win_size) * dt + start
+    hfo_bw = (hfo_band[1] - hfo_band[0]) / 2
+    hfo_center = (hfo_band[0] + hfo_band[1]) / 2
+    hfo_filtered = slepian_projection(lfp_flat, hfo_bw, 1 / dt, w0=hfo_center, min_conc=0.96)
+    uv_text = nice_unit_text('uv')
+
+    # The wavelet name "cmorB-C" encodes:
+    # B for bandwidth (Gaussian width): larger BW --> tighter time concentration
+    # C for center freq (cycle rate): higher frequency --> greater number of cycles within Gaussian window
+    cwt = CWT(hfo_time,
+              signal=lfp_flat,
+              scales=np.arange(1, win_size // 2),
+              wavelet='cmor1-1'
+              # wavelet='cmor1-1.5'
+              )
+    if normalize:
+        n_window = int((hfo_window[0] - start) / dt)
+        cwt_mu = np.abs(cwt.coefs[..., :n_window]).mean(axis=1, keepdims=True)
+        cwt.coefs /= cwt_mu
+
+    f = gridspec.figure
+    ax_full = f.add_subplot(gridspec[0])
+    ax_full.plot(hfo_time, lfp)
+    ax_full.set_ylabel(u'LFP ' + uv_text)
+    ax_hfo = f.add_subplot(gridspec[1], sharex=ax_full)
+    ax_hfo.plot(hfo_time, hfo_filtered)
+    ax_hfo.set_ylabel(u'HFO ' + uv_text)
+    ax_sg = f.add_subplot(gridspec[2], sharex=ax_full)
+    cws(cwt,
+        # coi=False,
+        coikw=dict(alpha=1.0, facecolor='gray'),
+        # cbar='horizontal',
+        cbar=None,
+        cmap='viridis',
+        cscale=('linear' if normalize else 'log'),
+        yscale='log',
+        yaxis='frequency',
+        ax=ax_sg)
+    ax_sg.set_title('')
+    clim = np.percentile(np.abs(cwt.coefs), [1, 99])
+    mesh = ax_sg.collections[0]
+    mesh.set_clim(*clim)
+    if max(ax_sg.get_ylim()) > 1e3:
+        ax_sg.set_ylim(top=1e3)
+    cb_ax = f.add_subplot(gridspec[3])
+    cb = f.colorbar(mesh, cax=cb_ax, orientation='horizontal')
+    # cb_ax = [ax for ax in f.axes if 'colorbar' in ax.get_label()]
+    t = 'Wavelet magnitude'
+    if normalize:
+        t = t + ' over baseline'
+    cb.set_label(t)
+    for ax in (ax_full, ax_hfo):
+        plotters.plt.setp(ax.get_xticklabels(), visible=False)
+        # ax.xaxis.set_major_formatter(NullFormatter())
+        ax.axvspan(hfo_start, hfo_end, facecolor='g', alpha=0.15, zorder=-10)
+    ax_sg.set_xlabel('Time (ms)')
+    gridspec.tight_layout(f, h_pad=0.5)
+    ax_sg.xaxis.set_major_formatter(_sec_to_msec)
+    # xticks = ax_sg.get_xticks()
+    # print('changing xticks from', xticks)
+    # ax_sg.set_xticklabels(['{:d}'.format(int(t * 1000)) for t in xticks])
+    return f
 
 
 class HFOCollection(SelectedFollowerCollection):
@@ -106,8 +192,6 @@ class HFOCollection(SelectedFollowerCollection):
 class ArrayAdapter(TabularAdapter):
 
     columns = [('start (ms)', 0), ('end (ms)', 1), ('data channel', 2)]
-    # Font fails with wx in OSX; see traitsui issue #13:
-    # font        = Font('Courier 10')
     alignment = 'left'
     format = '%d'
 
@@ -120,8 +204,11 @@ class ArrayAdapter(TabularAdapter):
     #         return '%0.2f'
 
 
-class HFOLocator(VisModule):
+class HFOLocator(PlotsInterval):
     name = Str('HFO Locator')
+    # Only plots new figures in gridspec format
+    new_figure = Bool(True)
+    _figstack = Instance(GridSpecStack, args=())
     file = File(exists=True)
     next = Button('Next event')
     prev = Button('Previous event')
@@ -140,14 +227,17 @@ class HFOLocator(VisModule):
     # window time in ms
     before_time = Range(low=-1000.0, high=0.0, value=-100.0)
     after_time = Range(low=0.0, high=1000.0, value=100.0)
+    hfo_band = Tuple(80.0, 500.0)  # Need to specify "N.0" for floats (otherwise will type as int)
+    normalize_scalogram = Bool(True)
+    plot_hfo = Button('plot HFO')
 
-    @on_trait_change('file')
-    def _read_table(self):
+    @observe('file')
+    def _read_table(self, event):
         df = pd.read_csv(self.file, header=None, names=['channel', 'start', 'end'])
         df = df.reindex(['start', 'end', 'channel'], axis=1)
         df.channel = df.channel - int(self.matlab_indexing)
         # keep start / end points as times rounded to nearest millisecond
-        offset = self.time_offset * 1000
+        offset = self.time_offset
         df.start = np.round(df.start + offset).astype('i')
         df.end = np.round(df.end + offset).astype('i')
         # If the data channels are in array order, then try to infer what the channels
@@ -175,12 +265,12 @@ class HFOLocator(VisModule):
         # self.last_idx = len(self.hfo_lookup)
         self.last_idx = len(start_times)
         # convert to regular ndarray because DataFrameEditor is broken :(
-        self._toggle_curves()
+        self._toggle_curves(None)
 
-    @on_trait_change('event_idx')
-    def move_to_event(self, event=None):
-        if event is not None:
-            self.trait_setq(event_idx = event)
+    @observe('event_idx, before_time, after_time')
+    def move_to_event(self, event, hfo_idx=None):
+        if hfo_idx is not None:
+            self.trait_setq(event_idx=hfo_idx)
         # ignore event if event_idx is indeterminate
         if self.event_idx < 0:
             return
@@ -198,8 +288,8 @@ class HFOLocator(VisModule):
         xrange = self.parent._qtwindow.p2.viewRange()[0]
         print('Range is: {:.2f}-{:.2f} (dx={:.2f})'.format(xrange[0], xrange[1], xrange[1] - xrange[0]))
 
-    @on_trait_change('show_hfos')
-    def _toggle_curves(self):
+    @observe('show_hfos')
+    def _toggle_curves(self, event):
         if not len(self.event_table):
             return
         if self.show_hfos:
@@ -211,13 +301,39 @@ class HFOLocator(VisModule):
             self.curve_manager.remove_curves('HFOs')
             del self.hfo_curves
 
+    def _plot_hfo_fired(self):
+        self.plot_current_hfo()
+
+    def plot_current_hfo(self):
+        if not len(self.event_table) or self.event_idx < 0:
+            return
+        hfo_start, hfo_end, channel = self.event_table[self.event_idx]
+        start = hfo_start + self.before_time
+        end = hfo_start + self.after_time
+        source = self.curve_manager.source_curve
+        page_start = int(self.page_limits[0] / source.dx)
+        start_idx = int(start / 1000 / source.dx) - page_start
+        end_idx = min(source.y_visible.shape[1], int(end / 1000 / source.dx) - page_start)
+        source_channel = source.plot_channels.index(channel)
+        hfo_segment = source.y_visible[source_channel, start_idx:end_idx] * 1e6
+        f, gs = self._get_fig(nrows=4, ncols=1, height_ratios=[1, 1, 3, 0.2], figsize=(4, 6))
+        print('Channel', source_channel, 'Full window:', start, end, 'HFO window:', hfo_start, hfo_end)
+        f = hfo_plot(hfo_segment, (start, end), (hfo_start, hfo_end), gs,
+                     hfo_band=self.hfo_band,
+                     normalize=self.normalize_scalogram)
+        try:
+            f.canvas.draw()
+        except:
+            pass
+
+
     def _time_offset_changed(self):
         if len(self.event_table):
             if hasattr(self, 'hfo_curves'):
                 self.curve_manager.remove_curves('HFOs')
                 del self.hfo_curves
             if self.file:
-                self._read_table()
+                self._read_table(None)
 
     def _change_event(self, sign):
         if 0 <= self.event_idx + sign < self.last_idx:
@@ -239,17 +355,20 @@ class HFOLocator(VisModule):
                                      scroll_to_row='event_idx')
         v = View(
             HGroup(
-                VGroup(Label('Times before/after event (ms)'),
-                       UItem('before_time'),
-                       UItem('after_time')),
                 VGroup(Label('Event file'),
                        UItem('file'),
                        HGroup(
                            VGroup(HGroup(UItem('matlab_indexing'), Label('1-based indexing?')),
-                                  HGroup(UItem('time_offset'), Label('Offset start time (s)'))),
+                                  HGroup(UItem('time_offset'), Label('Offset start time (ms)'))),
                            VGroup(HGroup(UItem('show_hfos'), Label('Highlight HFOs')))
                                # HGroup(UItem('array_channels'), Label('Array channel numbering?')),
                        )),
+                VGroup(Label('Times before/after event (ms)'),
+                       UItem('before_time'),
+                       UItem('after_time'),
+                       HGroup(Label('HFO band'), UItem('hfo_band', style='simple')),
+                       HGroup(Label('Normalize Scgram'), UItem('normalize_scalogram')),
+                       UItem('plot_hfo')),
                 VGroup(
                     UItem('event_table', editor=event_editor),
                     HGroup(UItem('next', enabled_when='event_idx < last_idx - 1'),
