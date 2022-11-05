915b707d03e87c935d88bf3a649d7c4693d6e3e8
miketrumpis <mtrumpis@gmail.com>
2022-11-05 11:29:28 -0700

    sonify: save WAV file and scroll through frames
    * The scrolling timer uses the AnimateInterval plugin, 
      which chops into the sound playback thread

--- fast_scroller/modules/sonify.py
+++ fast_scroller/modules/sonify.py
@@ -1,9 +1,14 @@
 import sounddevice as sd
 import numpy as np
+import os
+from scipy.io.wavfile import write as write_wav
+from typing import Tuple
 from multiprocessing import Process
-from traits.api import Button, Enum, Bool, Float
-from traitsui.api import View, HGroup, VGroup, UItem, Item, Label, EnumEditor, RangeEditor
+from threading import Thread
+from traits.api import Button, Enum, Bool, Float, File
+from traitsui.api import View, HGroup, VGroup, UItem, Item, Label, FileEditor, RangeEditor
 from .base import VisModule
+from . import AnimateInterval
 
 __all__ = ['SonifySeries']
 
@@ -11,14 +16,20 @@ __all__ = ['SonifySeries']
 class SonifySeries(VisModule):
     name = 'Sonify waveforms'
     play = Button('Play sound')
+    save = Button('Save sound')
     speed_up = Enum(1, (0.25, 0.5, 1, 2, 4))
     average = Bool(False)
+    scroll_frames = Bool(True)
+    wav_file = File(os.path.join(os.path.abspath(os.curdir), 'audio.wav'))
     volume = Float(1)
 
     def _play_fired(self):
         self.playback()
 
-    def playback(self):
+    def _save_fired(self):
+        self.save_audio()
+
+    def get_audio(self) -> Tuple[np.ndarray, float]:
         x, y = self.curve_manager.interactive_curve.current_data(visible=True, full_xdata=True)
         if self.average:
             sound_wave = y.mean(axis=0)
@@ -31,11 +42,25 @@ class SonifySeries(VisModule):
         fs = 1 / self.curve_manager.interactive_curve.dx
         sound_wave = sound_wave - sound_wave.min()
         sound_wave = sound_wave / sound_wave.max() * self.volume
+        return sound_wave, fs
 
-        sd.play(sound_wave, samplerate=fs * self.speed_up)
-        # proc = Process(target=sd.play, args=(sound_wave,), kwargs=dict(samplerate=fs * self.speed_up))
-        # proc.start()
+    def save_audio(self):
+        sound_wave, fs = self.get_audio()
+        write_wav(self.wav_file, int(np.round(fs)), sound_wave.astype('f'))
 
+    def playback(self):
+        sound_wave, fs = self.get_audio()
+        # proc = Process(target=sd.play, args=(sound_wave,), kwargs=dict(samplerate=int(fs * self.speed_up)))
+        proc = Thread(target=sd.play, args=(sound_wave,), kwargs=dict(samplerate=int(fs * self.speed_up)))
+        if self.scroll_frames:
+            # Make an Animator to sweep across the current frames (??) while sound plays
+            animator = AnimateInterval(parent=self.parent,
+                                       chan_map=self.chan_map,
+                                       curve_manager=self.curve_manager,
+                                       anim_time_scale=1.0 / self.speed_up)
+            animator._anim_frame_fired()
+        # sd.play(sound_wave, samplerate=int(np.round(fs * self.speed_up)))
+        proc.start()
 
     def default_traits_view(self):
         v = View(
@@ -51,6 +76,12 @@ class SonifySeries(VisModule):
                 VGroup(
                     Item('volume', label='Volume', editor=RangeEditor(low=0, high=1)),
                     UItem('play')
+                ),
+                Item('scroll_frames', label='Scroll through frames'),
+                VGroup(
+                    Item('wav_file', label='WAV File',
+                         editor=FileEditor(dialog_style='save')),
+                    Item('save', label='Save file')
                 )
             )
         )
